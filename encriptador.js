2193b2f7b229871186258cc88fc4009173619d85
David Zahid Riveros Hernández <150861435+Elgona9@users.noreply.github.com>
2024-07-23 22:41:28 -0500

    Último commit, solución de bugs.
    Solución de bugs, permite carácteres especiales como ! ? ¿ #, etc. así mismo permite números. Lo único que se excluyen son las mayúsculas y las letras con acento.

--- encriptador.js
+++ encriptador.js
@@ -70,8 +70,8 @@ function desencriptarTexto(){
 
 function validadorIntento(){ //Función que se asegura de que el texto introducido sea válido
     let texto = document.getElementById('campoTexto').value;
-    //expresión regular que solo coincide con letras minúsculas y espacios, no admite carácteres especiales
-    let expresion = /^[a-z\s]+$/;
+    let expresion = /^[a-z0-9\s!\"$%&'()*+,\-./:;<=>?@[\\\]^_`{|}~]+$/; //Expresión regular que permite letras minúsculas, números y caracteres especiales, más no permite mayúsculas 
+    //ni acentos
     if(!expresion.test(texto)){
         //Si el texto no es válido, muestra un mensaje de alerta
         alert('Por favor, introduce solo letras minúsculas y espacios.');
